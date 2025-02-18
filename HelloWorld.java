a328b0f44b8abf407962f250b26a7dc39720c074
Varun <93326999+VarunKvK@users.noreply.github.com>
2025-02-18 17:02:44 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args){
+   
+        System.out.println("The perfect square numbers are:"+"\n");
+        
+    }
+}
